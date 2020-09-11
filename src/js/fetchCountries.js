46725abdf489a3a13f691194d06e5fcea72adc05
Ivan Fesenko <pharmivan@gmail.com>
2020-09-11 11:41:57 +0300

    local save list added

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,14 @@
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-    .then(response => {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(
+    response => {
       if (response.ok) {
         return response.json();
       }
-    })
-    .catch(err => console.log(err));
+      throw new Error(`Ошибка по адресу ${url}, 
+      статус ошибки ${response.status}!`);
+    },
+  );
+  // .catch(err => {
+  //   throw new Error(err);
+  // });
 }
--- src/js/scrypt.js
+++ src/js/scrypt.js
@@ -1,5 +1,6 @@
 // import './notify';
-import fetchCountries from './fetchCountries';
+// import fetchCountries from './fetchCountries';
+import { alert, notice, info, success, error } from '@pnotify/core';
 import selectListTemplate from '../templates/countrySelectList.hbs';
 import selectedCountryTemplate from '../templates/selectedCountry.hbs';
 
@@ -9,11 +10,25 @@ const refs = {
   countrySelectList: document.querySelector('.country-select__list'),
 };
 const debounce = require('lodash.debounce');
-
 refs.input.addEventListener('keyup', debounce(onChangeInput, 500));
+
+function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Ошибка по адресу ${url}, 
+      статус ошибки ${response.status}!`);
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+}
 
 function onChangeInput(event) {
   clearSelectList();
+  clearSelectedCountry();
   const val = event.target.value;
 
   if (!val.length) return;
@@ -21,19 +36,36 @@ function onChangeInput(event) {
 }
 
 function processResult(countries) {
+  //delete
   console.log(countries);
+  //delete
   if (countries.length === 1) {
     generetaSelectedCountry(countries[0]);
   } else if (countries.length > 1 && countries.length < 11) {
+    refs.countriesList = countries;
     generateCountrySelectList(countries);
   } else {
     console.log('error');
+    const myError = error({
+      text: "I'm an error message.",
+    });
+    console.log(myError);
   }
 }
 
 function generateCountrySelectList(data) {
   const layout = selectListTemplate(data);
   refs.countrySelectList.insertAdjacentHTML('beforeend', layout);
+  refs.countrySelectList.addEventListener('click', onSelectCountry);
+}
+
+function onSelectCountry(event) {
+  if (event.target.nodeName !== 'LI') {
+    return;
+  }
+  refs.input.value = event.target.textContent;
+  clearSelectedCountry();
+  generetaSelectedCountry(refs.countriesList[event.target.dataset.index]);
 }
 
 function generetaSelectedCountry(data) {
@@ -43,4 +75,8 @@ function generetaSelectedCountry(data) {
 
 function clearSelectList() {
   refs.countrySelectList.innerHTML = '';
+}
+
+function clearSelectedCountry() {
+  refs.selectedCountry.innerHTML = '';
 }
